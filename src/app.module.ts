a1c6c16b50fd48946b87b0ccc232439e24e78e84
biscuitdey <biswashreedey@gmail.com>
2024-06-20 15:41:28 +0530

    Add config to allow api connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
 import { CustomerVerifierModule } from './customerVerifier/customerVerifier.module';
 import { PrismaService } from './shared/prisma/prisma.service';
+import { AutomapperModule } from '@automapper/nestjs';
+import { classes } from '@automapper/classes';
 
 @Module({
-  imports: [CustomerVerifierModule],
+  imports: [
+    CustomerVerifierModule,
+    AutomapperModule.forRoot({
+      strategyInitializer: classes(),
+    }),
+  ],
   providers: [PrismaService],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(
-    new ValidationPipe({
-      transform: true,
-    }),
-  );
-  app.enableCors();
+  // app.useGlobalPipes(
+  //   new ValidationPipe({
+  //     transform: true,
+  //   }),
+  // );
+  //app.enableCors();
   await app.listen(3000);
 }
 bootstrap();
